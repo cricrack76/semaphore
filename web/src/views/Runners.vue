82921d4f1106b02df6bbe734a88a51520621bfb3
Denis Gukov <denguk@gmail.com>
2024-11-14 22:43:07 +0500

    feat(pro): upgrade button

--- web/src/views/Runners.vue
+++ web/src/views/Runners.vue
@@ -140,7 +140,11 @@
       style="border-radius: 0;"
     >
       This feature available only in <v-chip small color="purple">PRO</v-chip> version.
-      <v-btn class="ml-2" color="purple" href="https://cloud.semaphoreui.com">Upgrade</v-btn>
+      <v-btn
+        class="ml-2"
+        color="purple"
+        href="https://cloud.semaphoreui.com/auth/login?new_project=premium"
+      >Upgrade</v-btn>
     </v-alert>
 
     <v-data-table
